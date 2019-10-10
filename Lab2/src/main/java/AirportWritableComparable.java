d09532a578711b9b0ca954d82d8e339cc898b1a1
AlisherRogov <rogov.alisher@gmail.com>
2019-10-10 22:47:43 +0300

    Scripted auto-commit on change (2019-10-10 22:47:43) by gitwatch.sh

--- Lab2/src/main/java/AirportWritableComparable.java
+++ Lab2/src/main/java/AirportWritableComparable.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 
 public class AirportWritableComparable implements WritableComparable {
 
+    private int a
+
     @Override
     public int compareTo(Object o) {
         return 0;
