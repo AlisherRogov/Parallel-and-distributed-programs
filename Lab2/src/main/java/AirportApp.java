7402de85368b416e4ba7ac231f5574266bf69872
AlisherRogov <rogov.alisher@gmail.com>
2019-10-10 21:27:30 +0300

    Scripted auto-commit on change (2019-10-10 21:27:30) by gitwatch.sh

--- Lab2/src/main/java/AirportApp.java
+++ Lab2/src/main/java/AirportApp.java
@@ -1,6 +1,4 @@
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -18,7 +16,8 @@ public class AirportApp {
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
        // job.setMapperClass(FlightMapper.class);
       //  job.setReducerClass(FlightReducer.class);
-        
+
+        job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
