0ed44abd08ad3578e2d0e9223fdabe95acae2a0c
AlisherRogov <rogov.alisher@gmail.com>
2019-10-10 22:47:26 +0300

    Scripted auto-commit on change (2019-10-10 22:47:26) by gitwatch.sh

--- Lab2/src/main/java/AirportWritableComparable.java
+++ Lab2/src/main/java/AirportWritableComparable.java
@@ -1,7 +1,23 @@
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class AirportWritableComparable implements WritableComparable {
 
-    private int airportId;
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
 
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
