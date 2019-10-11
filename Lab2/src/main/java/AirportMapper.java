7e22dbfc8c5e5d3bf757536a4487b67934b018fc
AlisherRogov <rogov.alisher@gmail.com>
2019-10-11 18:20:11 +0300

    Scripted auto-commit on change (2019-10-11 18:20:11) by gitwatch.sh

--- Lab2/src/main/java/AirportMapper.java
+++ Lab2/src/main/java/AirportMapper.java
@@ -11,7 +11,8 @@ public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableCom
         if(key.get() > 0) {
             String[] columns = value.toString().split("\",");
 
-            
+            int airportID = columns[0].
+
         }
     }
 }
