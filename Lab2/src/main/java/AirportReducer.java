aa9f4568c5e8b7593e6719d192dcc02c19274ebc
AlisherRogov <rogov.alisher@gmail.com>
2019-10-11 20:51:29 +0300

    Scripted auto-commit on change (2019-10-11 20:51:29) by gitwatch.sh

--- Lab2/src/main/java/AirportReducer.java
+++ Lab2/src/main/java/AirportReducer.java
@@ -8,6 +8,6 @@ public class AirportReducer extends Reducer<AirportWritableComparable, Text, Tex
 
     @Override
     protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        Iterator<Text> iter = 
+        Iterator<Text> iter = values.iterator();
     }
 }
