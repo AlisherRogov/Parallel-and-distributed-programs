510ce22288c1d3c82ed1018ec8f9f1521425a452
AlisherRogov <rogov.alisher@gmail.com>
2019-10-11 19:08:15 +0300

    Scripted auto-commit on change (2019-10-11 19:08:15) by gitwatch.sh

--- Lab2/src/main/java/FlightMapper.java
+++ Lab2/src/main/java/FlightMapper.java
@@ -10,7 +10,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComp
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if(key.get() > 0) {
             String[] columns = value.toString().split(",");
-            int airportID = Integer.parseInt(columns[14].replaceAll("\"", ""));
+            if(columns[14].equals(""))
             Text airportName = new Text(columns[1].replaceAll("\"", ""));
             context.write(new AirportWritableComparable(airportID, 0), airportName);
 
