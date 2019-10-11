eedacb253dcb9c44678e65c71ba7f29592d90ca1
AlisherRogov <rogov.alisher@gmail.com>
2019-10-11 20:25:44 +0300

    Scripted auto-commit on change (2019-10-11 20:25:44) by gitwatch.sh

--- Lab2/src/main/java/AirportGroupingComparator.java
+++ Lab2/src/main/java/AirportGroupingComparator.java
@@ -1,7 +1,7 @@
 
 import org.apache.hadoop.io.WritableComparator;
 
-public class AirportComparator implements WritableComparator {
+public class AirportComparator extends WritableComparator {
 
     @Override
     public int compare(Object o, Object t1) {
