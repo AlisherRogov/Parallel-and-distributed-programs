e8f318726cb034f6f08ff672253bef4c663c87f7
AlisherRogov <rogov.alisher@gmail.com>
2019-10-11 20:05:21 +0300

    Scripted auto-commit on change (2019-10-11 20:05:21) by gitwatch.sh

--- Lab2/src/main/java/AirportPartitioner.java
+++ Lab2/src/main/java/AirportPartitioner.java
@@ -5,6 +5,6 @@ public class AirportPartitioner extends Partitioner<AirportWritableComparable, T
 
     @Override
     public int getPartition(AirportWritableComparable k, Text v, int i) {
-        return 
+        return k.getAirportId() 
     }
 }
