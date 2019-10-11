963e32b90db13a23c0109cbfa64c16ba3c2cb2e9
AlisherRogov <rogov.alisher@gmail.com>
2019-10-11 19:56:01 +0300

    Scripted auto-commit on change (2019-10-11 19:56:01) by gitwatch.sh

--- Lab2/src/main/java/AirportApp.java
+++ Lab2/src/main/java/AirportApp.java
@@ -22,6 +22,7 @@ public class AirportApp {
         job.setPartitionerClass(TextPair.FirstPartitioner.class);
         job.setGroupingComparatorClass(TextPair.FirstComparator.class);
         job.setReducerClass(JoinReducer.class);
+
         job.setMapOutputKeyClass(TextPair.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
