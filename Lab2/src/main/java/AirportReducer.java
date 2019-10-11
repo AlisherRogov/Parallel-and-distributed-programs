3ec97593d417ff3b838dd930b99e2ac1004aadeb
AlisherRogov <rogov.alisher@gmail.com>
2019-10-11 20:51:12 +0300

    Scripted auto-commit on change (2019-10-11 20:51:12) by gitwatch.sh

--- Lab2/src/main/java/AirportReducer.java
+++ Lab2/src/main/java/AirportReducer.java
@@ -2,11 +2,12 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class AirportReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
 
     @Override
     protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-
+        Iterator
     }
 }
