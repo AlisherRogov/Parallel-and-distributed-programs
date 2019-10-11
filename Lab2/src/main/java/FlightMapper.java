63697c31df36df44bc2ddb867a1242b6c1f1bd7a
AlisherRogov <rogov.alisher@gmail.com>
2019-10-11 18:34:44 +0300

    Scripted auto-commit on change (2019-10-11 18:34:44) by gitwatch.sh

--- Lab2/src/main/java/FlightMapper.java
+++ Lab2/src/main/java/FlightMapper.java
@@ -2,5 +2,18 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if(key.get() > 0) {
+            String[] columns = value.toString().split("\",");
+            int airportID = Integer.parseInt(columns[0].replaceAll("\"", ""));
+            Text airportName = new Text(columns[1].replaceAll("\"", ""));
+            context.write(new AirportWritableComparable(airportID, 0), airportName);
+
+        }
+    }
 }
