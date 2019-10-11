2c46ff2f2f9b46fd2f05f8a49d4e8d5aeb773609
AlisherRogov <rogov.alisher@gmail.com>
2019-10-11 16:56:10 +0300

    Scripted auto-commit on change (2019-10-11 16:56:10) by gitwatch.sh

--- Lab2/src/main/java/AirportMapper.java
+++ Lab2/src/main/java/AirportMapper.java
@@ -1,10 +1,13 @@
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
 
     @Override
-    protected void map (LongWritable, Text, )
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }
