e0e315d3d2c8081be4c30b54e8c3bddcb6a74f15
AlisherRogov <rogov.alisher@gmail.com>
2019-10-11 20:10:55 +0300

    Scripted auto-commit on change (2019-10-11 20:10:55) by gitwatch.sh

--- Lab2/src/main/java/AirportReducer.java
+++ Lab2/src/main/java/AirportReducer.java
@@ -2,5 +2,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class AirportReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
+
     
 }
