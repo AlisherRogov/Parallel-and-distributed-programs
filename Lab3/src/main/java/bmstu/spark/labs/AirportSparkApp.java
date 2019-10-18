d676bb60dccd5e678fc7e8ce54f25b3faec76ba7
AlisherRogov <rogov.alisher@gmail.com>
2019-10-18 20:18:56 +0300

    Scripted auto-commit on change (2019-10-18 20:18:56) by gitwatch.sh

--- Lab3/src/main/java/bmstu/spark/labs/AirportSparkApp.java
+++ Lab3/src/main/java/bmstu/spark/labs/AirportSparkApp.java
@@ -14,7 +14,9 @@ public class AirportSparkApp {
         JavaRDD<String> airportData = sc.textFile("L_AIRPORT_ID.csv");
 
         JavaPairRDD<Integer, String> airportTable = airportData.mapToPair(
-                s -> 
+                s -> {
+                    CsvParse csvS = new CsvParse(s);
+                }
         );
     }
 }
