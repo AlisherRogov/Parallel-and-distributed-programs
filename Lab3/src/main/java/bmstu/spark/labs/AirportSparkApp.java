fd0289b90c144cb0c8eecabb7676d1629627e0b8
AlisherRogov <rogov.alisher@gmail.com>
2019-10-18 19:48:34 +0300

    Scripted auto-commit on change (2019-10-18 19:48:34) by gitwatch.sh

--- Lab3/src/main/java/bmstu/spark/labs/AirportSparkApp.java
+++ Lab3/src/main/java/bmstu/spark/labs/AirportSparkApp.java
@@ -8,6 +8,6 @@ public class AirportSparkApp {
         SparkConf conf  = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> flightTable = sc.textFile(664600583_T_ONTIME_sample.csv)
+        JavaRDD<String> flightTable = sc.textFile("664600583_T_ONTIME_sample.csv")
     }
 }
