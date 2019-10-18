8e7830064d27d3f72b6a8bf3b36c153006923c1d
AlisherRogov <rogov.alisher@gmail.com>
2019-10-18 19:48:45 +0300

    Scripted auto-commit on change (2019-10-18 19:48:45) by gitwatch.sh

--- Lab3/src/main/java/bmstu/spark/labs/AirportSparkApp.java
+++ Lab3/src/main/java/bmstu/spark/labs/AirportSparkApp.java
@@ -1,6 +1,7 @@
 package bmstu.spark.labs;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportSparkApp {
