5edd06a70e3d866e25b60710dcc9de720d9b2511
AlisherRogov <rogov.alisher@gmail.com>
2019-10-19 01:18:57 +0300

    Scripted auto-commit on change (2019-10-19 01:18:57) by gitwatch.sh

--- Lab3/src/main/java/bmstu/spark/labs/DelayStatistic.java
+++ Lab3/src/main/java/bmstu/spark/labs/DelayStatistic.java
@@ -19,5 +19,5 @@ public class DelayStatistic implements Serializable {
             this.countFlights = 1;
         }
 
-        public void addValue(int )
+        public void addValue(int statistic, float delayTime, boolean )
 }
