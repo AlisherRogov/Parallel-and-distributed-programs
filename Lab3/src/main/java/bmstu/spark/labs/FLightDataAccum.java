aab446beb70cd839d84f81666f1983bd887c9849
AlisherRogov <rogov.alisher@gmail.com>
2019-10-19 00:10:19 +0300

    Scripted auto-commit on change (2019-10-19 00:10:19) by gitwatch.sh

--- Lab3/src/main/java/bmstu/spark/labs/FLightDataAccum.java
+++ Lab3/src/main/java/bmstu/spark/labs/FLightDataAccum.java
@@ -5,7 +5,8 @@ import java.io.Serializable;
 public class FLightDataAccum implements Serializable {
 
     private static final int TYPE_FLIGHT = 1;
-    private static final int AIRPORT_ID = 14;
+    
+    private static final int DEST_AIRPORT_ID = 14;
     private static final int FLIGHT_DELAY_TIME = 18;
 
     private CsvParse csv;
