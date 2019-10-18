f25019e1ee218c38ecd14efefec03056f6f8fccd
AlisherRogov <rogov.alisher@gmail.com>
2019-10-19 01:45:58 +0300

    Scripted auto-commit on change (2019-10-19 01:45:58) by gitwatch.sh

--- Lab3/src/main/java/bmstu/spark/labs/DelayStatistic.java
+++ Lab3/src/main/java/bmstu/spark/labs/DelayStatistic.java
@@ -34,13 +34,15 @@ public class DelayStatistic implements Serializable {
         return countFlights;
     }
 
-    public DelayStatistic addValue(DelayStatistic statistic, float delayTime, boolean countCanceledFlights, boolean countDelayFlights) {
+    public DelayStatistic addStatistics(DelayStatistic statistic, float delayTime, boolean countCanceledFlights, boolean countDelayFlights) {
         return new DelayStatistic(Math.max(delayTime, statistic.getDelayTime()),
                 statistic.getCountCanceledFlights() + (countCanceledFlights ? 1 : 0),
                 statistic.getCountDelayFlights() + (countDelayFlights ? 1 : 0),
                 statistic.getCountFlights() + 1);
     }
 
-    public DelayStatistic combineTwoStatis
+    public DelayStatistic combineTwoStatistics(DelayStatistic a, DelayStatistic b) {
+            
+    }
 
 }
