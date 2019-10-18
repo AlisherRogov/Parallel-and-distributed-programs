1636a086dee631a0c8cee97ee310a38ea696a72a
AlisherRogov <rogov.alisher@gmail.com>
2019-10-18 19:40:55 +0300

    Scripted auto-commit on change (2019-10-18 19:40:55) by gitwatch.sh

--- Lab3/src/main/java/bmstu/spark/labs/AirportSparkApp.java
+++ Lab3/src/main/java/bmstu/spark/labs/AirportSparkApp.java
@@ -4,6 +4,6 @@ import org.apache.spark.SparkConf;
 
 public class AirportSparkApp {
     public static void main(String[] args) {
-        SparkConf conf  = new SparkConf().
+        SparkConf conf  = new SparkConf().setAppName()
     }
 }
