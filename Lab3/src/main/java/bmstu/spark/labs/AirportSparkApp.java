639354288270fcd9431effd695802530d6995f27
AlisherRogov <rogov.alisher@gmail.com>
2019-10-19 11:35:59 +0300

    Scripted auto-commit on change (2019-10-19 11:35:59) by gitwatch.sh

--- Lab3/src/main/java/bmstu/spark/labs/AirportSparkApp.java
+++ Lab3/src/main/java/bmstu/spark/labs/AirportSparkApp.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
 import scala.Tuple2;
 
 import java.util.Map;
@@ -47,6 +48,7 @@ public class AirportSparkApp {
                 DelayStatistic::combineTwoStatistics);
 
         Map<Integer, String> mapAirportTable = airportTable.collectAsMap();
-        
+
+        Broadcast
     }
 }
