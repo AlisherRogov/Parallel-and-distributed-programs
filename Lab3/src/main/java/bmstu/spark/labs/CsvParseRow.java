8482c8fb0f622a72deb2e7ec8765be2cc383da3f
AlisherRogov <rogov.alisher@gmail.com>
2019-10-24 17:54:59 +0300

    Scripted auto-commit on change (2019-10-24 17:54:59) by gitwatch.sh

--- Lab3/src/main/java/bmstu/spark/labs/CsvParseRow.java
+++ Lab3/src/main/java/bmstu/spark/labs/CsvParseRow.java
@@ -7,6 +7,7 @@ public class CsvParseRow {
     private static final int ORIGIN_AIRPORT_ID = 11;
     private static final int DEST_AIRPORT_ID = 14;
     private static final int FLIGHT_DELAY_TIME = 18;
+    private static final int CANCELED = 19;
     private static final int AIRPORT_ID = 0;
     private static final int AIRPORT_NAME = 1;
     private static final int TYPE_AIRPORT = 0;
@@ -25,11 +26,11 @@ public class CsvParseRow {
     }
 
     public int getAirportsID() {
-        return Integer.parseInt(replaceRegex(columns[AIRPORT_ID], "\"")); // в отдельную
+        return Integer.parseInt(replaceRegex(columns[AIRPORT_ID], "\""));
     }
 
     public String getAirportsName() {
-        return replaceRegex(columns[AIRPORT_NAME], "\"")); // в отдельную
+        return replaceRegex(columns[AIRPORT_NAME], "\"");
     }
 
     public int getOriginAirportId() {
