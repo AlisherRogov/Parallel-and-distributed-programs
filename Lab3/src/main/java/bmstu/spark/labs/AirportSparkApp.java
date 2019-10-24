53d61772b6141ffa33bf4e9e29b621f9f79c8832
AlisherRogov <rogov.alisher@gmail.com>
2019-10-24 17:41:52 +0300

    Scripted auto-commit on change (2019-10-24 17:41:52) by gitwatch.sh

--- Lab3/src/main/java/bmstu/spark/labs/AirportSparkApp.java
+++ Lab3/src/main/java/bmstu/spark/labs/AirportSparkApp.java
@@ -20,7 +20,7 @@ public class AirportSparkApp {
 
         JavaPairRDD<Integer, String> airportTable = airportData.mapToPair(
                 s -> {
-                    CsvParseRow csvS = new CsvParse(s);
+                    CsvParseRow csvS = new CsvParseRow(s);
                     return new Tuple2<>(csvS.getAirportsID(),
                             csvS.getAirportsName());
                 }
