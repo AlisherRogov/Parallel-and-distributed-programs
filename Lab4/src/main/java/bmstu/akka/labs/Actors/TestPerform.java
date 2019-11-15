f5bdc03113cfe16e43c1a0c077348a5a7e49a15f
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 21:21:40 +0300

    Scripted auto-commit on change (2019-11-15 21:21:40) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/Actors/TestPerform.java
+++ Lab4/src/main/java/bmstu/akka/labs/Actors/TestPerform.java
@@ -41,7 +41,7 @@ public class TestPerform extends AbstractActor {
         } catch (NoSuchMethodException e) {
             return "No such method :" + e.getLocalizedMessage();
         }
-        storeActor.tell(new TestsResultStorage(test.getPackageID()), description)
+        storeActor.tell(new TestsResultStorage(test.getPackageID(), description), 
     }
 
 }
