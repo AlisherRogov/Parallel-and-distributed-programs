e6a4e83401932160ea95178ebf6f239f0366acd8
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 21:57:13 +0300

    Scripted auto-commit on change (2019-11-15 21:57:12) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/Actors/TestsRouter.java
+++ Lab4/src/main/java/bmstu/akka/labs/Actors/TestsRouter.java
@@ -2,13 +2,14 @@ package bmstu.akka.labs.Actors;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.Props;
 
 public class TestsRouter extends AbstractActor {
     private ActorRef storeActor;
     private ActorRef testPerfomRouter;
 
     public TestsRouter() {
-        this.storeActor = getContext().actorOf();
+        this.storeActor = getContext().actorOf(Props.create());
         this.testPerfomRouter = testPerfomRouter;
     }
 
