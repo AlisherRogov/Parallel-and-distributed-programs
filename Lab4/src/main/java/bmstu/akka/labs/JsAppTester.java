520915c6b4610627b4448da8bce693828c7c5280
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 12:30:01 +0300

    Scripted auto-commit on change (2019-11-15 12:30:01) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/JsAppTester.java
+++ Lab4/src/main/java/bmstu/akka/labs/JsAppTester.java
@@ -8,7 +8,7 @@ public class JsAppTester {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("test");
         ActorRef storeActor = system.actorOf(Props.create(StoreActor.class));
-        
+
 
     }
 }
