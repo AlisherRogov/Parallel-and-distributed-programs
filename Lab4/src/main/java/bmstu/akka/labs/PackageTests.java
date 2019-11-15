fdc743f309bd2d2ae7e658f546cb44ebce5d53c0
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 23:27:05 +0300

    Scripted auto-commit on change (2019-11-15 23:27:05) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/PackageTests.java
+++ Lab4/src/main/java/bmstu/akka/labs/PackageTests.java
@@ -13,5 +13,5 @@ public class PackageTests {
     @JsonProperty("jsScript") private String jsScript;
     @JsonProperty("functionName") private String functionName;
     @JsonProperty("tests") private List<SingleTest> tests = null;
-    @JsonIgnore private Map<String, Object> un
+    @JsonIgnore private Map<String, Object> unexpected
 }
