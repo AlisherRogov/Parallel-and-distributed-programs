36cb21521f1e370954ea9e630e6888d8b0631ec0
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 23:27:16 +0300

    Scripted auto-commit on change (2019-11-15 23:27:16) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/PackageTests.java
+++ Lab4/src/main/java/bmstu/akka/labs/PackageTests.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,5 +14,5 @@ public class PackageTests {
     @JsonProperty("jsScript") private String jsScript;
     @JsonProperty("functionName") private String functionName;
     @JsonProperty("tests") private List<SingleTest> tests = null;
-    @JsonIgnore private Map<String, Object> unexpectedProperties
+    @JsonIgnore private Map<String, Object> unexpectedProperties = new HashMap<>();
 }
