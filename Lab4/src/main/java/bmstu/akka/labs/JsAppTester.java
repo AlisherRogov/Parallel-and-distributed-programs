e48e02e068452205c05156074b76e296015db6fe
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 22:36:08 +0300

    Scripted auto-commit on change (2019-11-15 22:36:08) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/JsAppTester.java
+++ Lab4/src/main/java/bmstu/akka/labs/JsAppTester.java
@@ -3,11 +3,28 @@ package bmstu.akka.labs;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 public class JsAppTester {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("Lab4");
-        ActorRef storeActor = system.actorOf(Props.create(StoreActor.class));
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        JsAppTester tester = new JsAppTester(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+
 
 
     }
