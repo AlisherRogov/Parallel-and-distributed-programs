f1e0152aea310107f90ce259be6175d676aeff31
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 19:55:33 +0300

    Scripted auto-commit on change (2019-11-15 19:55:33) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/Actors/TestPerform.java
+++ Lab4/src/main/java/bmstu/akka/labs/Actors/TestPerform.java
@@ -10,6 +10,7 @@ import javax.script.ScriptEngineManager;
 public class TestPerform extends AbstractActor {
     private ActorRef storeActor;
 
+    private static 
     ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
     engine.(jscript);
     Invocable invocable = (Invocable) engine;
