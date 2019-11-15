53baf5e823851686d52f4e13a596823ee0c86c7e
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 23:31:28 +0300

    Scripted auto-commit on change (2019-11-15 23:31:28) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/SingleTest.java
+++ Lab4/src/main/java/bmstu/akka/labs/SingleTest.java
@@ -2,8 +2,10 @@ package bmstu.akka.labs;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 public class SingleTest {
     @JsonProperty("testName") private String testName;
     @JsonProperty("expectedResults") private String expectedResults;
-    @JsonProperty("params") private 
+    @JsonProperty("params") private List<Object>
 }
