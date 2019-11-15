034f278a8912d84cb264e1e4ad4d84e0f56aba88
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 13:15:19 +0300

    Scripted auto-commit on change (2019-11-15 13:15:19) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/Actors/TestsResultStorage.java
+++ Lab4/src/main/java/bmstu/akka/labs/Actors/TestsResultStorage.java
@@ -12,6 +12,6 @@ public class TestsResultStorage extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(StoreMess)
+                .match(StoreMessage.class)
     }
 }
