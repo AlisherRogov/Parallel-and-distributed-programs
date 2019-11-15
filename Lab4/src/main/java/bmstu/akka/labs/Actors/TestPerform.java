177ccd22685247bce80fbc7efd3e6ac9b95f739a
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 20:04:43 +0300

    Scripted auto-commit on change (2019-11-15 20:04:43) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/Actors/TestPerform.java
+++ Lab4/src/main/java/bmstu/akka/labs/Actors/TestPerform.java
@@ -6,12 +6,13 @@ import akka.actor.ActorRef;
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class TestPerform extends AbstractActor {
     private ActorRef storeActor;
 
     private static String performScript(String functionName, String script,
-                                        Object... args) throws  {
+                                        Object... args) throws ScriptException,  {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
         engine.eval("jscript");
         Invocable invocable = (Invocable) engine;
