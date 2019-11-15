186c85b80b53b5666255f8200271379e1d0dfbf4
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 13:13:44 +0300

    Scripted auto-commit on change (2019-11-15 13:13:44) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/Actors/TestsResultStorage.java
+++ Lab4/src/main/java/bmstu/akka/labs/Actors/TestsResultStorage.java
@@ -2,8 +2,9 @@ package bmstu.akka.labs.Actors;
 
 import akka.actor.AbstractActor;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class TestsResultStorage extends AbstractActor {
-    private Map<String, String> store 
+    private Map<String, String> store = new HashMap<>()
 }
