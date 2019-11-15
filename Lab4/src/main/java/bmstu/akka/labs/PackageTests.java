021ea6832ab60b71cf1d92bbb6158f5c67abb4c5
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 23:26:08 +0300

    Scripted auto-commit on change (2019-11-15 23:26:08) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/PackageTests.java
+++ Lab4/src/main/java/bmstu/akka/labs/PackageTests.java
@@ -1,6 +1,7 @@
 package bmstu.akka.labs;
 
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
@@ -10,5 +11,5 @@ public class PackageTests {
     @JsonProperty("jsScript") private String jsScript;
     @JsonProperty("functionName") private String functionName;
     @JsonProperty("tests") private List<SingleTest> tests = null;
-    @JsonProperty
+    @JsonAnyGetter
 }
