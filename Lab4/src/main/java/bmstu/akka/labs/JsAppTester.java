2bec371ec93d3182cec800eef20cbc5cb3e21509
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 23:54:12 +0300

    Scripted auto-commit on change (2019-11-15 23:54:12) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/JsAppTester.java
+++ Lab4/src/main/java/bmstu/akka/labs/JsAppTester.java
@@ -50,7 +50,7 @@ public class JsAppTester {
                         })
                 ),
                 get(() ->
-                        parameter("packageId"))
+                        parameter("packageId", packageID))
 
         );
     }
