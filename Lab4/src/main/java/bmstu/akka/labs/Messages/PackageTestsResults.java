b4faeca74bff8f41299baef3f57bb17092e2461e
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 17:03:48 +0300

    Scripted auto-commit on change (2019-11-15 17:03:48) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/Messages/PackageTestsResults.java
+++ Lab4/src/main/java/bmstu/akka/labs/Messages/PackageTestsResults.java
@@ -3,8 +3,9 @@ package bmstu.akka.labs.Messages;
 import java.util.ArrayList;
 
 public class PackageTestsResults {
-    private int packageID
+    private int packageID;
     private ArrayList<String> results;
 
+    
 
 }
