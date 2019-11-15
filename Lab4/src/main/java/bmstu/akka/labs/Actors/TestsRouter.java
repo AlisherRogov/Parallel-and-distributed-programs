9f050c25e86115e7f83bb449d9338cbc01f3b9fd
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 23:38:03 +0300

    Scripted auto-commit on change (2019-11-15 23:38:03) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/Actors/TestsRouter.java
+++ Lab4/src/main/java/bmstu/akka/labs/Actors/TestsRouter.java
@@ -33,8 +33,8 @@ public class TestsRouter extends AbstractActor {
     }
 
     private  void receivePackageTests(PackageTests tests) {
-        Stream<JsFunction> jsFunctionStream = Stream.of(tests.getTests())
-                .map(test -> new JsFunction(tests.getPackageID(), ));
+        Stream.of(tests.getTests())
+                .map(test -> new JsFunction(tests.getPackageID(),))
     }
 
 
