4dddd0caa6a97eb9e9499d6c3b25ed9a5956075f
AlisherRogov <rogov.alisher@gmail.com>
2019-11-15 23:31:37 +0300

    Scripted auto-commit on change (2019-11-15 23:31:37) by gitwatch.sh

--- Lab4/src/main/java/bmstu/akka/labs/SingleTest.java
+++ Lab4/src/main/java/bmstu/akka/labs/SingleTest.java
@@ -7,5 +7,6 @@ import java.util.List;
 public class SingleTest {
     @JsonProperty("testName") private String testName;
     @JsonProperty("expectedResults") private String expectedResults;
-    @JsonProperty("params") private List<Object> params
+    @JsonProperty("params") private List<Object> params = null;
+    
 }
