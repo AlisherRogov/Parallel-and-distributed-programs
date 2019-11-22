44d5148d343c9c5d683155a3c5809a407a0b1798
AlisherRogov <rogov.alisher@gmail.com>
2019-11-23 01:50:22 +0300

    Scripted auto-commit on change (2019-11-23 01:50:22) by gitwatch.sh

--- Lab5/src/main/java/bmstu/akka/labs/Actors/StorageActor.java
+++ Lab5/src/main/java/bmstu/akka/labs/Actors/StorageActor.java
@@ -9,7 +9,7 @@ public class StorageActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder
                 .create()
-                .match(GetTestResult.class,  )
+                .match(GetTestResult.class, msg ->  )
                 .match(StoreTestResult.class)
     }
 }
