bbb19db3e72034d30484f121692c385ff5027b9d
AlisherRogov <rogov.alisher@gmail.com>
2019-11-23 00:17:17 +0300

    Scripted auto-commit on change (2019-11-23 00:17:17) by gitwatch.sh

--- Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
+++ Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
@@ -1,6 +1,8 @@
 package bmstu.akka.labs;
 
+import java.io.IOException;
+
 public class StressTestingApp {
 
-    public static void main(String[] args )
+    public static void main(String[] args) throws IOException
 }
