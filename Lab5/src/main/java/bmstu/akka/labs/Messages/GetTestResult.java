bdffcd10baf6908e4a2c5e57ba4d0cb01757244f
AlisherRogov <rogov.alisher@gmail.com>
2019-11-23 01:48:55 +0300

    Scripted auto-commit on change (2019-11-23 01:48:55) by gitwatch.sh

--- Lab5/src/main/java/bmstu/akka/labs/Messages/GetTestResult.java
+++ Lab5/src/main/java/bmstu/akka/labs/Messages/GetTestResult.java
@@ -2,5 +2,18 @@ package bmstu.akka.labs.Messages;
 
 public class GetTestResult {
     private String Url;
-    private int 
+    private int count;
+
+    public  GetTestResult(String Url, int count) {
+        this.Url = Url;
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getUrl() {
+        return Url;
+    }
 }
