8e9845416173064dcdd79e31cb417ca398291569
AlisherRogov <rogov.alisher@gmail.com>
2019-11-23 02:45:54 +0300

    Scripted auto-commit on change (2019-11-23 02:45:54) by gitwatch.sh

--- Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
+++ Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
@@ -26,7 +26,7 @@ public class StressTestingApp {
 
     public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create("routes");
-        ActorRef store = system.actorOf()
+        ActorRef store = system.actorOf(StorageActor.)
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
