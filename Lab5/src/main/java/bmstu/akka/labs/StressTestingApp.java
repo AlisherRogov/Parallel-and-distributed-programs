5a6c33495851d086696cf0a7275f86dc8816b7b7
AlisherRogov <rogov.alisher@gmail.com>
2019-11-23 01:15:17 +0300

    Scripted auto-commit on change (2019-11-23 01:15:17) by gitwatch.sh

--- Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
+++ Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
@@ -23,7 +23,7 @@ public class StressTestingApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-       final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = Flow.of(Http)
+       final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = Flow.of(HttpRequest)
 
 
 
