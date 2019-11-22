c3339ced0c38e68856d16b78160ece0ae3f80b0b
AlisherRogov <rogov.alisher@gmail.com>
2019-11-23 00:59:31 +0300

    Scripted auto-commit on change (2019-11-23 00:59:31) by gitwatch.sh

--- Lab5/src/main/java/bmstu/akka/labs/Actors/StorageActor.java
+++ Lab5/src/main/java/bmstu/akka/labs/Actors/StorageActor.java
@@ -5,5 +5,7 @@ import akka.actor.AbstractActor;
 public class StorageActor extends AbstractActor {
 
     @Override
-    public Receive createReceive
+    public Receive createReceive() {
+        
+    }
 }
