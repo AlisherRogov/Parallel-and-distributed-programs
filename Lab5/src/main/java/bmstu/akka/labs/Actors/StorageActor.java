b86e2e878c0135a6be2d5b56cdba6614a67ecc0a
AlisherRogov <rogov.alisher@gmail.com>
2019-11-23 02:07:10 +0300

    Scripted auto-commit on change (2019-11-23 02:07:10) by gitwatch.sh

--- Lab5/src/main/java/bmstu/akka/labs/Actors/StorageActor.java
+++ Lab5/src/main/java/bmstu/akka/labs/Actors/StorageActor.java
@@ -4,6 +4,7 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class StorageActor extends AbstractActor {
     private Map<String, Integer> storage = new HashMap<>();
