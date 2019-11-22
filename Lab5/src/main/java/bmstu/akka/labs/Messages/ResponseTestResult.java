b7b9033260bb86e2c3e1f8f5c9ec1e79c0d2b820
AlisherRogov <rogov.alisher@gmail.com>
2019-11-23 02:28:47 +0300

    Scripted auto-commit on change (2019-11-23 02:28:47) by gitwatch.sh

--- Lab5/src/main/java/bmstu/akka/labs/Messages/ResponseTestResult.java
+++ Lab5/src/main/java/bmstu/akka/labs/Messages/ResponseTestResult.java
@@ -3,7 +3,21 @@ package bmstu.akka.labs.Messages;
 public class ResponseTestResult {
     private boolean hasInfo;
     private String Url;
-    private Long 
+    private Long time;
 
-    public ResponseTestResult
+    public ResponseTestResult(boolean hasInfo, String Url, Long time) {
+        this.hasInfo = hasInfo;
+        this.Url = Url;
+        this.time = time;
+    }
+
+    public boolean 
+
+    public String getUrl() {
+        return Url;
+    }
+
+    public Long getTime() {
+        return time;
+    }
 }
