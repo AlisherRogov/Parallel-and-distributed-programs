b1cadf3aa610179b49aff152391f04ed1233bc2a
AlisherRogov <rogov.alisher@gmail.com>
2019-11-23 01:35:46 +0300

    Scripted auto-commit on change (2019-11-23 01:35:46) by gitwatch.sh

--- Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
+++ Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
@@ -27,7 +27,8 @@ public class StressTestingApp {
 
        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = Flow.of(HttpRequest.class)
                .map(request -> {
-                       String url = request.getUri().query().get("testURL").orElse("");
+                       String url = request.getUri().query().get("testURL").orElse("ufc.com");
+                       
                })
 
 
