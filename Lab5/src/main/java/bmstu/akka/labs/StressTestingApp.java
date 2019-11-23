dc5f9fc629eb761d75a396b143c51e4e9e19c5af
AlisherRogov <rogov.alisher@gmail.com>
2019-11-23 04:02:41 +0300

    Scripted auto-commit on change (2019-11-23 04:02:41) by gitwatch.sh

--- Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
+++ Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
@@ -64,7 +64,9 @@ public class StressTestingApp {
                                            ));
 
                                });
-               }).map(response ->)
+               }).map(response -> {
+                   
+               })
 
 
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
