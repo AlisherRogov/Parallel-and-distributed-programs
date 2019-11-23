09c8c9c989f2d237f44844a50204d6eb25e8a310
AlisherRogov <rogov.alisher@gmail.com>
2019-11-23 03:44:24 +0300

    Scripted auto-commit on change (2019-11-23 03:44:24) by gitwatch.sh

--- Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
+++ Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
@@ -81,7 +81,7 @@ public class StressTestingApp {
                 .mapConcat(msg -> Collections.nCopies(msg.getCount(), msg.getUrl()))
                 .mapAsync(4,  pair -> {
                     Long startTime = System.currentTimeMillis();
-                    return 
+                    return Htt
                 })
     }
         .
