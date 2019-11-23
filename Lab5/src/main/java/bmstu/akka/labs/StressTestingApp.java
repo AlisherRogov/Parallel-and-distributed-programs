f1d2702efe5c4b9d1e9b7a4e3bc854c3610d199a
AlisherRogov <rogov.alisher@gmail.com>
2019-11-23 04:14:08 +0300

    Scripted auto-commit on change (2019-11-23 04:14:08) by gitwatch.sh

--- Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
+++ Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
@@ -68,7 +68,7 @@ public class StressTestingApp {
                        StoreTestResult storeTestResult = new StoreTestResult(response.getUrl(), response.getTime());
                        storeRef.tell(storeTestResult, ActorRef.noSender());
                    }
-                   return HttpResponse
+                   return akka.http.javadsl.model.HttpResponse
                })
 
 
