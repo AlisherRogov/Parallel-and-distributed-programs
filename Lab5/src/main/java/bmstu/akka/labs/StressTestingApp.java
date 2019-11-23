343d2e5d9a70e8aa0e20f3271771ddcfcbeca3c2
AlisherRogov <rogov.alisher@gmail.com>
2019-11-23 03:36:06 +0300

    Scripted auto-commit on change (2019-11-23 03:36:06) by gitwatch.sh

--- Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
+++ Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
@@ -76,5 +76,5 @@ public class StressTestingApp {
 
     }
 
-    static Sink<ResponseTestResult, CompletionStage<Long>> testSink = 
+    static Sink<ResponseTestResult, CompletionStage<Long>> testSink = return 
 }
