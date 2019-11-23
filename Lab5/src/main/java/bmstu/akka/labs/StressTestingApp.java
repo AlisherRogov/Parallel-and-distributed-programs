75597b757805d77693d1a9e9321f3dd6d3ab782a
AlisherRogov <rogov.alisher@gmail.com>
2019-11-23 03:13:07 +0300

    Scripted auto-commit on change (2019-11-23 03:13:07) by gitwatch.sh

--- Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
+++ Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
@@ -52,7 +52,8 @@ public class StressTestingApp {
                                    if(response.isCounted()) {
                                        return CompletableFuture.completedFuture(response);
                                    } else {
-                                       Source.from(Collections.singleton(pair)
+                                       Source.from(Collections.singleton(pair))
+                                               .
                                    }
                                }
                                        )
