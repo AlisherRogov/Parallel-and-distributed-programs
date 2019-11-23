69e59acfee1020319e1bac83bd2a6673b83faa5b
AlisherRogov <rogov.alisher@gmail.com>
2019-11-23 03:43:13 +0300

    Scripted auto-commit on change (2019-11-23 03:43:13) by gitwatch.sh

--- Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
+++ Lab5/src/main/java/bmstu/akka/labs/StressTestingApp.java
@@ -79,7 +79,9 @@ public class StressTestingApp {
     static Sink<GetTestResult, CompletionStage<Long>> testSink() {
         return Flow.<GetTestResult>create()
                 .mapConcat(msg -> Collections.nCopies(msg.getCount(), msg.getUrl()))
-                .mapAsync(4,  pair -> )
+                .mapAsync(4,  pair -> {
+                    
+                })
     }
         .
 }
