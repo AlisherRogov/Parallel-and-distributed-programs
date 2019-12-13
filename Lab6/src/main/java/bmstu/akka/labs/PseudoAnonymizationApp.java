78513a4a2a88fbc6d45320e2a17c17316d97909e
AlisherRogov <rogov.alisher@gmail.com>
2019-12-14 00:47:15 +0300

    Scripted auto-commit on change (2019-12-14 00:47:15) by gitwatch.sh

--- Lab6/src/main/java/bmstu/akka/labs/PseudoAnonymizationApp.java
+++ Lab6/src/main/java/bmstu/akka/labs/PseudoAnonymizationApp.java
@@ -40,7 +40,7 @@ public class PseudoAnonymizationApp {
                 ConnectHttp.toHost(hostUri.getHost().toString(), hostUri.getPort()),
                 materializer
         );
-        System.out.println("Server online at" + Press RETURN to stop...");
+        System.out.println("Server online at" + hostUri.getHost().toString() + ':' + hostUri.getPort() + "Press RETURN to stop...");
         System.in.read();
         binding
                 .thenCompose(ServerBinding::unbind)
