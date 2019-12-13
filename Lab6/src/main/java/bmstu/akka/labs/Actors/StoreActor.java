193bf2ecdd611a775175971a813d324b92f01b47
AlisherRogov <rogov.alisher@gmail.com>
2019-12-13 22:41:19 +0300

    Scripted auto-commit on change (2019-12-13 22:41:19) by gitwatch.sh

--- Lab6/src/main/java/bmstu/akka/labs/Actors/StoreActor.java
+++ Lab6/src/main/java/bmstu/akka/labs/Actors/StoreActor.java
@@ -3,6 +3,7 @@ package bmstu.akka.labs.Actors;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 import bmstu.akka.labs.Messages.GetMessage;
+import bmstu.akka.labs.Messages.ResponseMessage;
 import bmstu.akka.labs.Messages.StoreMessage;
 
 import java.util.Random;
@@ -19,6 +20,6 @@ public class StoreActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(StoreMessage.class, msg -> addresses = msg.getAddresses())
-                .match(GetMessage.class, msg -> sender().tell(new )
+                .match(GetMessage.class, msg -> sender().tell(new ResponseMessage())
     }
 }
