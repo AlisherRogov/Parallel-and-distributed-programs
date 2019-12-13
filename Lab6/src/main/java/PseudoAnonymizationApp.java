12ed28084d658d6fce9fbf3f680ca37f878d1a39
AlisherRogov <rogov.alisher@gmail.com>
2019-12-13 21:32:35 +0300

    Scripted auto-commit on change (2019-12-13 21:32:35) by gitwatch.sh

--- Lab6/src/main/java/PseudoAnonymizationApp.java
+++ Lab6/src/main/java/PseudoAnonymizationApp.java
@@ -5,6 +5,6 @@ import akka.actor.Props;
 public class PseudoAnonymizationApp {
     public static void main (String[] args) {
         ActorSystem system = ActorSystem.create("anonymizer");
-        ActorRef storeActor = system.actorOf(Props.create(Stor))
+        ActorRef storeActor = system.actorOf(Props.create(Storage))
     }
 }
