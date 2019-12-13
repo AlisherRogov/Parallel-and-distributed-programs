02d82d5e39d654486793a166765943e5d7082df2
AlisherRogov <rogov.alisher@gmail.com>
2019-12-13 22:59:33 +0300

    Scripted auto-commit on change (2019-12-13 22:59:33) by gitwatch.sh

--- Lab6/src/main/java/bmstu/akka/labs/ServerRoutes.java
+++ Lab6/src/main/java/bmstu/akka/labs/ServerRoutes.java
@@ -16,7 +16,7 @@ public class ServerRoutes {
 
     public Route getRoutes() {
         return route (
-                get(() -> parameter())
+                get(() -> parameter("url"))
         )
     }
 }
