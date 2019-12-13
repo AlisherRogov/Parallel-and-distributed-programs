8d63e71e6cfcdcdc99bea58d4238442e867a7666
AlisherRogov <rogov.alisher@gmail.com>
2019-12-13 22:37:40 +0300

    Scripted auto-commit on change (2019-12-13 22:37:40) by gitwatch.sh

--- Lab6/src/main/java/bmstu/akka/labs/Actors/StoreActor.java
+++ Lab6/src/main/java/bmstu/akka/labs/Actors/StoreActor.java
@@ -15,6 +15,6 @@ public class StoreActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
-        return ReceiveBuilder
+        return ReceiveBuilder.create()
     }
 }
