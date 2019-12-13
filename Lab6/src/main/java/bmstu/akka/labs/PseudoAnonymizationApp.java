21dfb7549f94f393fbb74721bf5b550d6f1638ec
AlisherRogov <rogov.alisher@gmail.com>
2019-12-14 00:44:53 +0300

    Scripted auto-commit on change (2019-12-14 00:44:53) by gitwatch.sh

--- Lab6/src/main/java/bmstu/akka/labs/PseudoAnonymizationApp.java
+++ Lab6/src/main/java/bmstu/akka/labs/PseudoAnonymizationApp.java
@@ -28,7 +28,7 @@ public class PseudoAnonymizationApp {
         Uri hostUri = Uri.create("http://" + hostAddress);
 
 
-        final ServersHandler serversHandler = new ServersHandler(zkAddress, storeActor, hostUri.getHost() )
+        final ServersHandler serversHandler = new ServersHandler(zkAddress, storeActor, hostUri.getHost().toString() + ':' + hostUri.getPort() )
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = serverRoutes
                 .getRoutes()
                 .flow(system, materializer);
