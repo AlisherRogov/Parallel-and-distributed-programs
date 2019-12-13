7a600a49c659285192e99f93d5630a209ee239ae
AlisherRogov <rogov.alisher@gmail.com>
2019-12-14 00:34:29 +0300

    Scripted auto-commit on change (2019-12-14 00:34:29) by gitwatch.sh

--- Lab6/src/main/java/bmstu/akka/labs/ServersHandler.java
+++ Lab6/src/main/java/bmstu/akka/labs/ServersHandler.java
@@ -44,7 +44,7 @@ public class ServersHandler {
                 .map(Optional::get)
                 .map(this::getNodeData)
                 .map(String::new)
-                .toArray()
+                .toArray(String[]::new)
     }
 
     private List<String> getChildren() {
