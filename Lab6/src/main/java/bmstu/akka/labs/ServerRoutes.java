dee339d7e94373c18403d573d0ae32467dce4149
AlisherRogov <rogov.alisher@gmail.com>
2019-12-13 22:54:06 +0300

    Scripted auto-commit on change (2019-12-13 22:54:06) by gitwatch.sh

--- Lab6/src/main/java/bmstu/akka/labs/ServerRoutes.java
+++ Lab6/src/main/java/bmstu/akka/labs/ServerRoutes.java
@@ -1,10 +1,11 @@
 package bmstu.akka.labs;
 
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.server.Route;
 
 public class ServerRoutes {
     private ActorSystem system;
-    peivate
+    private ActorRef
     Route getRoutes()
 }
