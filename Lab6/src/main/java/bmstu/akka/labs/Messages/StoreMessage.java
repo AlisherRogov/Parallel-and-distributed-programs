c91102009c199988942b563031b9c491e88432ae
AlisherRogov <rogov.alisher@gmail.com>
2019-12-13 22:36:25 +0300

    Scripted auto-commit on change (2019-12-13 22:36:25) by gitwatch.sh

--- Lab6/src/main/java/bmstu/akka/labs/Messages/StoreMessage.java
+++ Lab6/src/main/java/bmstu/akka/labs/Messages/StoreMessage.java
@@ -2,5 +2,8 @@ package bmstu.akka.labs.Messages;
 
 public class StoreMessage {
     private final String[] addresses;
-    
+
+    public StoreMessage(String[] addresses) {
+        this.addresses = addresses;
+    }
 }
