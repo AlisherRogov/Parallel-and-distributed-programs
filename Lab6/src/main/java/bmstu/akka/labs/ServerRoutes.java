830966430453a96c5ec26a925d58fb96b7e1ef90
AlisherRogov <rogov.alisher@gmail.com>
2019-12-13 23:15:20 +0300

    Scripted auto-commit on change (2019-12-13 23:15:20) by gitwatch.sh

--- Lab6/src/main/java/bmstu/akka/labs/ServerRoutes.java
+++ Lab6/src/main/java/bmstu/akka/labs/ServerRoutes.java
@@ -43,7 +43,7 @@ public class ServerRoutes {
 
     private CompletionStage<HttpResponse> redirectRequest(String url, int count) {
        FutureConverters.toJava( Patterns.ask(storeActor, new GetMessage(), 5000))
-               .thenApply()
+               .thenApply(o -> )
                .
     }
 }
