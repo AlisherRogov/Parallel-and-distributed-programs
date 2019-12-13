e2a0e579d5f181b90c254016dc3199d2790c68db
AlisherRogov <rogov.alisher@gmail.com>
2019-12-14 00:22:56 +0300

    Scripted auto-commit on change (2019-12-14 00:22:56) by gitwatch.sh

--- Lab6/src/main/java/bmstu/akka/labs/ServersHandler.java
+++ Lab6/src/main/java/bmstu/akka/labs/ServersHandler.java
@@ -20,6 +20,8 @@ public class ServersHandler {
 
     private void watchConnections(WatchedEvent event) {
         if(event.getState() == Watcher.Event.KeeperState.Expired ||
-                event.getState() = Watcher.Event.KeeperState.Disconnected)
+                event.getState() = Watcher.Event.KeeperState.Disconnected) {
+            try
+        }
     }
 }
