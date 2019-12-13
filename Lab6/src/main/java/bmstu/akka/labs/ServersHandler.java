9fed8c9b98fe2240eda3fa402eff59d1eabd64a3
AlisherRogov <rogov.alisher@gmail.com>
2019-12-14 00:20:23 +0300

    Scripted auto-commit on change (2019-12-14 00:20:23) by gitwatch.sh

--- Lab6/src/main/java/bmstu/akka/labs/ServersHandler.java
+++ Lab6/src/main/java/bmstu/akka/labs/ServersHandler.java
@@ -3,14 +3,16 @@ package bmstu.akka.labs;
 import akka.actor.ActorRef;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.io.IOException;
+
 public class ServersHandler {
     private final String zkAddress;
     private final ActorRef storeActor;
     private ZooKeeper zoo;
 
-    public ServersHandler(String zkAddress, ActorRef storeActor, String address) {
+    public ServersHandler(String zkAddress, ActorRef storeActor, String address) throws IOException {
         this.zkAddress = zkAddress;
         this.storeActor = storeActor;
-        zoo  = new ZooKeeper(zkAddress, 5000, )
+        zoo  = new ZooKeeper(zkAddress, 5000, this);
     }
 }
