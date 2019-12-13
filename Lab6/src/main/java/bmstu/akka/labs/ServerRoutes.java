f4dcaad411de807d3f1c5df66b4310dc173c520d
AlisherRogov <rogov.alisher@gmail.com>
2019-12-13 23:24:30 +0300

    Scripted auto-commit on change (2019-12-13 23:24:30) by gitwatch.sh

--- Lab6/src/main/java/bmstu/akka/labs/ServerRoutes.java
+++ Lab6/src/main/java/bmstu/akka/labs/ServerRoutes.java
@@ -39,7 +39,7 @@ public class ServerRoutes {
         return route (
                 get(() -> parameter("url",  url -> parameter("count", countString -> {
                     int count = stringToInt(countString);
-                    return count == 0 ? completeWithFuture(requestUrl(url)) :  completeWithFuture()
+                    return count == 0 ? completeWithFuture(requestUrl(url)) :  completeWithFuture(redirectRequest())
                 })))
         );
     }
