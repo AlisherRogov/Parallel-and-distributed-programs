cdc713cb14e01e3e263a31ae0a036d958f4e2d19
AlisherRogov <rogov.alisher@gmail.com>
2019-12-13 22:37:15 +0300

    Scripted auto-commit on change (2019-12-13 22:37:15) by gitwatch.sh

--- Lab6/src/main/java/bmstu/akka/labs/Actors/StoreActor.java
+++ Lab6/src/main/java/bmstu/akka/labs/Actors/StoreActor.java
@@ -2,11 +2,13 @@ package bmstu.akka.labs.Actors;
 
 import akka.actor.AbstractActor;
 
+import java.util.Random;
+
 public class StoreActor extends AbstractActor {
     private String[] addresses;
-    private 
+    private Random random;
 
     public StoreActor() {
-
+        this.random = new Random();
     }
 }
