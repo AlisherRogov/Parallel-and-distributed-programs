e1da1f620dfff4fa5029062158466fbc52bcd049
AlisherRogov <rogov.alisher@gmail.com>
2019-12-26 00:59:14 +0300

    Scripted auto-commit on change (2019-12-26 00:59:13) by gitwatch.sh

--- Lab7/src/main/java/bmstu/zmq/labs/Client.java
+++ Lab7/src/main/java/bmstu/zmq/labs/Client.java
@@ -4,14 +4,17 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
         ZContext context = new ZContext();
         ZMQ.Socket socket = context.createSocket(SocketType.REQ);
         socket.connect("tcp://localhost:5555");
+        Scanner scanner = new Scanner();
 
         while(!Thread.currentThread().isInterrupted()){
-            String 
+            String
         }
 
         context.destroySocket(socket);
