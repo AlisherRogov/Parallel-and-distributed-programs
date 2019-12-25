eead28338e5da5d29f1dfe66e37522ea296a6522
AlisherRogov <rogov.alisher@gmail.com>
2019-12-26 00:45:11 +0300

    Scripted auto-commit on change (2019-12-26 00:45:11) by gitwatch.sh

--- Lab7/src/main/java/bmstu/zmq/labs/Proxy.java
+++ Lab7/src/main/java/bmstu/zmq/labs/Proxy.java
@@ -30,7 +30,7 @@ public class Proxy {
         socket.bind("tcp://localhost:5555");
         System.out.println("bind");
 
-        while
+        while()
         context.destroySocket(socket);
         context.destroy();
     }
