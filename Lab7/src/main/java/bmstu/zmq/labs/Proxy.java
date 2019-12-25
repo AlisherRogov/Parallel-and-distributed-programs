cb3b49f320f4daa4fd3c26a307014783d72942ed
AlisherRogov <rogov.alisher@gmail.com>
2019-12-26 02:13:17 +0300

    Scripted auto-commit on change (2019-12-26 02:13:17) by gitwatch.sh

--- Lab7/src/main/java/bmstu/zmq/labs/Proxy.java
+++ Lab7/src/main/java/bmstu/zmq/labs/Proxy.java
@@ -12,30 +12,25 @@ public class Proxy {
 
     public static void main(String[] args) {
         ZContext context = new ZContext();
-        ZMQ.Socket socket = context.createSocket(SocketType.REP);
-        socket.bind("tcp://localhost:5555");
-        System.out.println("bind");
-//        
-//        ZMQ.Socket backend = context.createSocket(SocketType.ROUTER);
-//        ZMQ.Socket frontend = context.createSocket(SocketType.ROUTER);
-//
-//        backend.bind(address);
-//        frontend.bind(ClientAddress);
-//
-//        ZMQ.Poller items = context.createPoller(2);
-//        items.register(frontend, ZMQ.Poller.POLLIN);
-//        items.register(backend, ZMQ.Poller.POLLIN);
-//
-//        while (!Thread.currentThread().isInterrupted()) {
-//
-//        }
 
 
-        while(!Thread.currentThread().isInterrupted()){
-            String req = socket.recvStr();
-            socket.send("reply " + req);
+        ZMQ.Socket backend = context.createSocket(SocketType.ROUTER);
+        ZMQ.Socket frontend = context.createSocket(SocketType.ROUTER);
+
+        backend.bind(address);
+        frontend.bind(ClientAddress);
+
+        ZMQ.Poller items = context.createPoller(2);
+        items.register(frontend, ZMQ.Poller.POLLIN);
+        items.register(backend, ZMQ.Poller.POLLIN);
+
+        while (!Thread.currentThread().isInterrupted()) {
+
         }
-        context.destroySocket(socket);
+
+
+
+
         context.destroy();
     }
 
