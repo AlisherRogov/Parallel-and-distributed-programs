fb0655c9ca937086dadadda95163ec15820e9f15
AlisherRogov <rogov.alisher@gmail.com>
2019-12-26 00:45:05 +0300

    Scripted auto-commit on change (2019-12-26 00:45:05) by gitwatch.sh

--- Lab7/src/main/java/bmstu/zmq/labs/Proxy.java
+++ Lab7/src/main/java/bmstu/zmq/labs/Proxy.java
@@ -29,11 +29,8 @@ public class Proxy {
         ZMQ.Socket socket = context.createSocket(SocketType.REP);
         socket.bind("tcp://localhost:5555");
         System.out.println("bind");
-        for (int i = 0; i < 10; i++) {
-            socket.send("request"+i, 0);
-            String reply = socket.recvStr();
-            System.out.println("reply" + i+ "result="+ reply);
-        }
+
+        while
         context.destroySocket(socket);
         context.destroy();
     }
