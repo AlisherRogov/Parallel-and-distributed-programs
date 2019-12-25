cefacd6b8a3388ae08eadc3ef13d0eb0ff048043
AlisherRogov <rogov.alisher@gmail.com>
2019-12-26 00:32:07 +0300

    Scripted auto-commit on change (2019-12-26 00:32:07) by gitwatch.sh

--- Lab7/src/main/java/bmstu/zmq/labs/Client.java
+++ Lab7/src/main/java/bmstu/zmq/labs/Client.java
@@ -1,10 +1,11 @@
 package bmstu.zmq.labs;
 
 import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
 
 public class Client {
     public static void main(String[] args) {
         ZContext context = new ZContext();
-        
+        ZMQ.Socket socket = new ZMQ.Socket();
     }
 }
