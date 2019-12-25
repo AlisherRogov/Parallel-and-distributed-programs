1fcb4d8b55e6c3a55d9f4822aceb0d0a4e584f02
AlisherRogov <rogov.alisher@gmail.com>
2019-12-26 02:11:19 +0300

    Scripted auto-commit on change (2019-12-26 02:11:19) by gitwatch.sh

--- Lab7/src/main/java/bmstu/zmq/labs/Client.java
+++ Lab7/src/main/java/bmstu/zmq/labs/Client.java
@@ -17,7 +17,7 @@ public class Client {
             String command = in.nextLine();
             Command cmd = new Command(command);
             socket.send(command, 0);
-            System.out.println(socket.recv());
+            System.out.println(socket.recvStr());
         }
 
         context.destroySocket(socket);
