07fadc2d2686b0465b19fadf75382530a6d16e41
AlisherRogov <rogov.alisher@gmail.com>
2019-12-25 22:33:42 +0300

    Scripted auto-commit on change (2019-12-25 22:33:42) by gitwatch.sh

--- Lab7/src/main/java/bmstu/zmq/labs/Cache.java
+++ Lab7/src/main/java/bmstu/zmq/labs/Cache.java
@@ -1,11 +1,12 @@
 package bmstu.zmq.labs;
 
+import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
 public class Cache {
     public static void main(String[] args) {
         ZContext context = new ZContext();
-        ZMQ.Socket socket = context.createSocket()
+        ZMQ.Socket socket = context.createSocket(SocketType.DEALER);
     }
 }
