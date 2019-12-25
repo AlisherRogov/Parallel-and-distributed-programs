9337352dc8d98f4365c1e0f06ddaa164b9d91709
AlisherRogov <rogov.alisher@gmail.com>
2019-12-26 00:44:13 +0300

    Scripted auto-commit on change (2019-12-26 00:44:13) by gitwatch.sh

--- Lab7/src/main/java/bmstu/zmq/labs/Proxy.java
+++ Lab7/src/main/java/bmstu/zmq/labs/Proxy.java
@@ -11,20 +11,21 @@ public class Proxy {
     private static String address;
 
     public static void main(String[] args) {
-        ZContext context = new ZContext();
-        ZMQ.Socket backend = context.createSocket(SocketType.ROUTER);
-        ZMQ.Socket frontend = context.createSocket(SocketType.ROUTER);
-
-        backend.bind(address);
-        frontend.bind(ClientAddress);
-
-        ZMQ.Poller items = context.createPoller(2);
-        items.register(frontend, ZMQ.Poller.POLLIN);
-        items.register(backend, ZMQ.Poller.POLLIN);
-
-        while (!Thread.currentThread().isInterrupted()) {
-
-        }
+//        ZContext context = new ZContext();
+//        ZMQ.Socket backend = context.createSocket(SocketType.ROUTER);
+//        ZMQ.Socket frontend = context.createSocket(SocketType.ROUTER);
+//
+//        backend.bind(address);
+//        frontend.bind(ClientAddress);
+//
+//        ZMQ.Poller items = context.createPoller(2);
+//        items.register(frontend, ZMQ.Poller.POLLIN);
+//        items.register(backend, ZMQ.Poller.POLLIN);
+//
+//        while (!Thread.currentThread().isInterrupted()) {
+//
+//        }
+        
     }
 
 }
