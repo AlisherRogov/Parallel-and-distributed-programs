d600ad757b7cd0c5551aeb8562e81ef6393fab64
AlisherRogov <rogov.alisher@gmail.com>
2019-12-25 22:49:34 +0300

    Scripted auto-commit on change (2019-12-25 22:49:34) by gitwatch.sh

--- Lab7/src/main/java/bmstu/zmq/labs/Proxy.java
+++ Lab7/src/main/java/bmstu/zmq/labs/Proxy.java
@@ -6,9 +6,17 @@ import org.zeromq.ZMQ;
 
 
 public class Proxy {
+
+    private static String ClientAddress;
+    private static String address;
+
     public static void main(String[] args) {
         ZContext context = new ZContext();
         ZMQ.Socket backend = context.createSocket(SocketType.ROUTER);
         ZMQ.Socket frontend = context.createSocket(SocketType.ROUTER);
+
+        backend.bind(address);
+        frontend.bind(ClientAddress);
     }
+
 }
