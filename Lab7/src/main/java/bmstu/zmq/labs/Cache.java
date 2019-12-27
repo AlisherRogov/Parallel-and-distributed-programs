ac31b9058cd0dd2302d828765ecb32b537d340a5
AlisherRogov <rogov.alisher@gmail.com>
2019-12-28 01:40:50 +0300

    Scripted auto-commit on change (2019-12-28 01:40:50) by gitwatch.sh

--- Lab7/src/main/java/bmstu/zmq/labs/Cache.java
+++ Lab7/src/main/java/bmstu/zmq/labs/Cache.java
@@ -22,7 +22,7 @@ public class Cache {
                 String cmd = msg.getLast().toString();
                 Command command = new Command(cmd);
 
-                if ()
+                if (command.getType() == )
             }
         }
 
