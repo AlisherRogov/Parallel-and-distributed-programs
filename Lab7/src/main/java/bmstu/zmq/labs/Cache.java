e5e8017c6600ea93ebd427d3287353054df9f0e4
AlisherRogov <rogov.alisher@gmail.com>
2019-12-28 01:32:57 +0300

    Scripted auto-commit on change (2019-12-28 01:32:57) by gitwatch.sh

--- Lab7/src/main/java/bmstu/zmq/labs/Cache.java
+++ Lab7/src/main/java/bmstu/zmq/labs/Cache.java
@@ -5,12 +5,12 @@ import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
 public class Cache {
-    private static String cacheAddress = "tcp://localhost:5569";
+    private static String CACHE_ADDRESS = "tcp://localhost:5569";
     public static void main(String[] args) {
         ZContext context = new ZContext();
         ZMQ.Socket socket = context.createSocket(SocketType.DEALER);
-        socket.connect(cacheAddress);
-        
+        socket.connect(CACHE_ADDRESS);
+
 
 
     }
