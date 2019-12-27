f57af5476adcabd33eecd938193a137f3b4051b2
AlisherRogov <rogov.alisher@gmail.com>
2019-12-28 01:44:34 +0300

    Scripted auto-commit on change (2019-12-28 01:44:34) by gitwatch.sh

--- Lab7/src/main/java/bmstu/zmq/labs/Cache.java
+++ Lab7/src/main/java/bmstu/zmq/labs/Cache.java
@@ -32,7 +32,7 @@ public class Cache {
                     Integer value = storage.get(key);
 
                     String response = value == null ? "null" : Integer.toString(value);
-                    msg.getLast()
+                    msg.getLast().reset();
 
                 }
             }
