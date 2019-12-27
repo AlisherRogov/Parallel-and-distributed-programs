7ab829cfd03fa3225390ac574b7aaf58cbf238bc
AlisherRogov <rogov.alisher@gmail.com>
2019-12-28 00:42:18 +0300

    Scripted auto-commit on change (2019-12-28 00:42:18) by gitwatch.sh

--- Lab7/src/main/java/bmstu/zmq/labs/Proxy.java
+++ Lab7/src/main/java/bmstu/zmq/labs/Proxy.java
@@ -34,7 +34,7 @@ public class Proxy {
                     int key = cmd.getIndex();
                     boolean isKeyValid = sendGet(key, msg, backend);
                     if(!isKeyValid) {
-                        
+                        sendToClient();
                     }
                 }
                 if (cmd.getType() == "PUT") {
