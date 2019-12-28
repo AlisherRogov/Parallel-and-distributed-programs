94db071ed4840187632a57eb7942ec5bbd8469e5
AlisherRogov <rogov.alisher@gmail.com>
2019-12-28 14:21:14 +0300

    Scripted auto-commit on change (2019-12-28 14:21:14) by gitwatch.sh

--- Lab7/src/main/java/bmstu/zmq/labs/Proxy.java
+++ Lab7/src/main/java/bmstu/zmq/labs/Proxy.java
@@ -90,9 +90,7 @@ public class Proxy {
         return false;
     }
 
-    private static boolean isActive(StorageInfo storageInfo) {
-
-    }
+    
 
     private static boolean sendPut(int key, ZMsg frame, ZMQ.Socket backend) {
         boolean isKeyValid = false;
