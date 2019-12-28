23e37106aaefaed9b5103439f665850f7498ecc1
AlisherRogov <rogov.alisher@gmail.com>
2019-12-28 12:47:20 +0300

    Scripted auto-commit on change (2019-12-28 12:47:20) by gitwatch.sh

--- Lab7/src/main/java/bmstu/zmq/labs/Client.java
+++ Lab7/src/main/java/bmstu/zmq/labs/Client.java
@@ -1,30 +1,80 @@
 package bmstu.zmq.labs;
 
+import bmstu.zmq.labs.Command;
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
+import org.zeromq.ZMsg;
 
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
+import java.io.Closeable;
+import java.util.logging.Logger;
 
-public class Client {
-    private static String CLIENT_ADDRESS = "tcp://localhost:5569";
-    public static void main(String[] args) {
-        ZContext context = new ZContext();
-        ZMQ.Socket socket = context.createSocket(SocketType.REQ);
-        socket.connect(CLIENT_ADDRESS);
-        Scanner in = new Scanner(System.in);
+public class Client implements Closeable{
+    private static final Logger log = Logger.getLogger(Client.class.getName());
+    ZContext context;
+    ZMQ.Socket Socket;
+    public static void main(String[] args) throws IOException {
+        try (
+                Scanner scanner = new Scanner(System.in);
+                ZContext context = new ZContext();
+                Client client = new Client(context)
+        ) {
+            client.connect("tcp://localhost:5569");
+            client.handle(scanner, System.out);
+        }
+    }
+    public Client(ZContext context) {
+        this.context = context;
+        this.Socket = context.createSocket(SocketType.REQ);
+    }
 
-        while(true) {
-            String command = in.nextLine();
-            System.out.println(command);
-            socket.send(command);
-            Command cmd = new Command(command);
-            if (cmd.getType().equals("GET")){
-                System.out.println(socket.recvStr());
+    private void connect(String clientAddr) {
+        Socket.connect(clientAddr);
+    }
+
+    private void handle(Scanner scanner, PrintStream out) {
+        while (!Thread.currentThread().isInterrupted()){
+            out.print("> ");
+            Command cmd = new Command(scanner.nextLine());
+            if(cmd.getType().equals("GET")){
+                String res = SendGetMsg(cmd);
+                out.println(res != null ? res : "ERROR");
+            } else if(cmd.getType().equals("PUT")){
+                String result = SendPutMsg(cmd);
+                out.println(result);
             }
         }
+    }
 
-   //     context.destroySocket(socket);
-     //   context.destroy();
+    private String SendPutMsg(Command putCmd) {
+        ZMsg msg = ZMsg.newStringMsg(putCmd.toString());
+        log.info("Sending message: " + msg);
+        msg.send(Socket);
+
+        ZMsg rec = ZMsg.recvMsg(Socket);
+        return rec.popString();
+    }
+
+    private String SendGetMsg(Command getCmd) {
+        ZMsg msg = ZMsg.newStringMsg(getCmd.toString());
+        log.info("Sending message: " + msg);
+        msg.send(Socket);
+
+        ZMsg rec = ZMsg.recvMsg(Socket);
+        log.info("Received message: " + rec);
+        Command cmd = new Command(rec.popString());
+        if (!cmd.getType().equals("ERROR")){
+            return rec.toString();
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        context.destroySocket(Socket);
     }
 }
